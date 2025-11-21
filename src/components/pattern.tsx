b1b11510763eaac73413f4a4eebb45fc675445db
Yoan Kilolo <YKilolo77@gmail.com>
2025-11-21 14:58:40 +0000

    african pattern (_for element div_)

--- src/components/pattern.tsx
+++ src/components/pattern.tsx
@@ -3,16 +3,10 @@ import React from 'react';
 export const Pattern = () => (
   <svg width="100%" height="100%" xmlns="http://www.w3.org/2000/svg">
     <defs>
-      <pattern id="pattern-squares" x="0" y="0" width="20" height="20" patternUnits="userSpaceOnUse">
-        <rect x="0" y="0" width="7" height="7" fill="hsl(var(--primary))" />
-      </pattern>
-      <pattern id="pattern-lines" x="0" y="0" width="20" height="20" patternUnits="userSpaceOnUse">
-        <path d="M 0 5 L 5 0 M 0 15 L 15 0 M 10 20 L 20 10" stroke="hsl(var(--primary))" strokeWidth="1" />
-      </pattern>
       <pattern id="pattern-combined" x="0" y="0" width="40" height="40" patternUnits="userSpaceOnUse">
         <rect x="0" y="0" width="40" height="40" fill="transparent" />
-        <rect x="0" y="0" width="15" height="15" fill="hsl(var(--primary))" />
-        <path d="M 20 25 L 25 20 M 20 35 L 35 20 M 30 40 L 40 30" stroke="hsl(var(--primary))" strokeWidth="1" />
+        <path d="M 10 0 V 10 H 0" fill="none" stroke="hsl(var(--primary))" strokeWidth="1"/>
+        <path d="M 30 40 V 30 H 40" fill="none" stroke="hsl(var(--primary))" strokeWidth="1"/>
       </pattern>
     </defs>
     <rect width="100%" height="100%" fill="url(#pattern-combined)" />
