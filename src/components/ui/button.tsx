b47c56df9b74d1702c4518dad748abd92d63391d
Yoan Kilolo <YKilolo77@gmail.com>
2025-11-21 14:52:04 +0000

    Peux til se remplir de jaune au hoover avec le texte qui change aussi de

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -13,7 +13,7 @@ const buttonVariants = cva(
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
-          "border border-accent bg-transparent text-accent hover:bg-accent/10",
+          "border border-accent bg-transparent text-accent hover:bg-accent hover:text-accent-foreground",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent/10 hover:text-accent-foreground",
